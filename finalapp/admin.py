528f1c789c9c2f1f3f439e61b539cbcb78fcb16a
Osiakosia <osvaldas.ok@gmail.com>
2025-09-25 13:41:39 +0300

    models registered in admin.py .

--- finalapp/admin.py
+++ finalapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Order, OrderLine, Car,Service ,CarModel
+
+admin.site.register(Order)
+admin.site.register(OrderLine)
+admin.site.register(Car)
+admin.site.register(Service)
+admin.site.register(CarModel)
+
